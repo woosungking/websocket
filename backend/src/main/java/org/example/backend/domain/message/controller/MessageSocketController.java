1809beb181f8abb54a5cafcea2b0cec42dbbc62a
kingmusung <abuga991008@gmail.com>
2024-09-19 21:26:35 +0900

    웹소켓 컨트롤러 적용

--- backend/src/main/java/org/example/backend/domain/message/controller/MessageSocketController.java
+++ backend/src/main/java/org/example/backend/domain/message/controller/MessageSocketController.java
@@ -1,4 +1,23 @@
 package org.example.backend.domain.message.controller;
 
+import org.example.backend.domain.message.dto.RequestMessage;
+import org.example.backend.domain.message.service.MessageService;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageSocketController {
+	private final MessageService messageService;
+
+	public MessageSocketController(MessageService messageService) {
+		this.messageService = messageService;
+	}
+
+	@MessageMapping("/{roomId}")
+	@SendTo("/room/{roomId}")
+	public void enter(@DestinationVariable("roomId") Long roomId, RequestMessage message){
+	messageService.saveMessage(message.getContent(), message.getSender(),roomId);
+	};
 }
